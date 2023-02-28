ae15bafed24f7ab3d1fea4665b8d44a90180575b
Andrey Marakulin <annndruha.github@gmail.com>
2023-02-28 20:55:54 +0300

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,19 +1,22 @@
-chrome.contextMenus.create({
-        'id': '7Ed2x9MAFz3dgdJkBsuS3UrX',
-        "title": 'Add to notes',
-        'type': 'normal',
-        'contexts': ['selection']
-    }, () => {}
-)
+chrome.contextMenus.removeAll(() => {
+    chrome.contextMenus.create({
+            'id': '7Ed2x9MAFz3dgdJkBsuS3UrX',
+            "title": 'Add to notes',
+            'type': 'normal',
+            'contexts': ['selection']
+        }, () => {
+        }
+    )
+})
 
 chrome.contextMenus.onClicked.addListener(
     (info, tab) => {
-        chrome.storage.sync.get(null, (res)=> {
+        chrome.storage.sync.get(null, (res) => {
             let idx = Object.keys(res).length + 1
 
             let json = {}
             json[idx] = info.selectionText
-            chrome.storage.sync.set(json, (res)=> {
+            chrome.storage.sync.set(json, (res) => {
                 console.log('json set', res)
             })
         })
